33e23c1d6594a3967ab582733a89f9301ed9ee67
Arpan Sarkar <arpan98sarkar@gmail.com>
2023-11-06 18:23:37 +0530

    displying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
 }
 interface FetchGamesResponse {
     count: number;
@@ -17,16 +24,17 @@ const useGames = () => {
 
     useEffect(() => {
         const controller = new AbortController();
-        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
+        apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
             .then((res) => {
                 setGames(res.data.results);
             })
             .catch(err => {
                 if (err instanceof CanceledError) return;
-                setError(err.message)});
+                setError(err.message)
+            });
 
-            return () => controller.abort();
-    },[]);
+        return () => controller.abort();
+    }, []);
     return { games, error };
 }
 
