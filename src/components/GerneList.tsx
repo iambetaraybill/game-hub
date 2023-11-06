37abce35f37157dbbeb5cc1e901ce3fc5e3ac173
Arpan Sarkar <arpan98sarkar@gmail.com>
2023-11-07 00:44:32 +0530

    Highlight genre

--- src/components/GerneList.tsx
+++ src/components/GerneList.tsx
@@ -5,9 +5,10 @@ import GameListSkeleton from './GameListSkeleton';
 
 interface Props{
     onSelectedGenre : ( genre: Genre) => void;
+    selectedGenre : Genre | null;
 }
 
-const GenreList = ({onSelectedGenre}: Props) => {
+const GenreList = ({onSelectedGenre, selectedGenre}: Props) => {
     const {data, isLoading, error} = useGenres();
     const skeleton = [1, 2, 3, 4, 5, 6,7,8,9];
     if(error) return null;
@@ -17,7 +18,7 @@ const GenreList = ({onSelectedGenre}: Props) => {
        {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
         <HStack>
             <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-            <Button onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+            <Button fontWeight={genre.id === selectedGenre?.id ? 'extrabold' : 'normal'} onClick={() => onSelectedGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
         </HStack>
         </ListItem>)} 
     </List>
