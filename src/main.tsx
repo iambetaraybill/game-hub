9c811e6c697feb84cb4c55b452ca594f3dfe742d
Arpan Sarkar <arpan98sarkar@gmail.com>
2023-11-07 19:44:47 +0530

    credit

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { Alert, AlertIcon, ChakraProvider, ColorModeScript, Link } from '@chakra-ui/react'
 import App from './App.tsx'
 import theme from './theme.ts'
 import './index.css'
+import { SiLinkfire } from 'react-icons/si'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+      <Alert status='info' color={'red'} fontSize='20px' justifyContent='center'>
+        <Link href='https://codewithmosh.com' isExternal>
+          Made with Mosh Hamedani 💎
+        </Link>
+      </Alert>
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
 )
