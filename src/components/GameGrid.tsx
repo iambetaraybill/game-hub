56094cef5ce737d0b69ebabecb1fc3158c494056
Arpan Sarkar <arpan98sarkar@gmail.com>
2023-11-07 19:14:01 +0530

    theme costomize

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,10 +14,8 @@ const GameGrid = ({gemeQuery}: Props) => {
 
     const { data, error, isLoading } = useGames(gemeQuery); 
     const skeletons = [1, 2, 3, 4, 5, 6];
-
+    if(error) return  <Text>{error}</Text>;
     return (
-        <>
-            {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' spacing={6}>
                 {isLoading && skeletons.map((ske) =>
                     <GameCardContainer key={ske}>
@@ -30,7 +28,6 @@ const GameGrid = ({gemeQuery}: Props) => {
                 )}
 
             </SimpleGrid>
-        </>
     )
 }
 
