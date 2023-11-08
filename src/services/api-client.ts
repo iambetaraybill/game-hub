06ef47b667b85944e1f8fda59166fe948da7287a
Arpan Sarkar <arpan98sarkar@gmail.com>
2023-11-08 18:00:11 +0530

    api keys

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params : {
-        key : '059dfcee26d84317b480f71ec48b7b63'
+        key : process.env.API_KEY
     }
 });
