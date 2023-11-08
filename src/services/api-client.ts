4936e570f59b1fc458429fad93c31999e3a6585a
Arpan Sarkar <arpan98sarkar@gmail.com>
2023-11-08 18:44:27 +0530

    APi key from env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params : {
-        key : process.env.API_KEY?.toString()
+        key : import.meta.env.VITE_API_KEY
     }
 });
